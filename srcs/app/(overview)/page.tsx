0f9ab17115fb670da4fd75bfa2efc5691279627a
Lorena Gracia <pendol888@gmail.com>
2026-07-21 17:38:46 +0000

    add(frontend): Dice component

--- srcs/app/(overview)/page.tsx
+++ srcs/app/(overview)/page.tsx
@@ -1,56 +1,44 @@
 "use client";
-import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 import PatternControl from "@/components/Pattern/PatternControl";
+import DiceScene from "@/components/DiceScene";
 import { useState } from "react";
 
 export default function Home() {
-const [paused, setPaused] = useState(false);
+  const [paused, setPaused] = useState(false);
+
   return (
     <>
-    <PatternControl
-        paused={paused}
-        onToggle={() => setPaused(!paused)}
-    />
-    <main className="container container-two">
-      <section className="column">
-        <Image
-          className="light"
-          src="/dice-mockup.svg"
-          alt="dice mockup"
-          width={200}
-          height={20}
-        />
-      </section>
-      <section className="column gap-8">
-        <div className="row">
-          <h1>
-            This is Dice
-          </h1>
-          <p>
-              Welcome
-          </p>
-        <div className="flex flex-row gap-4 text-base font-medium">
-          <Link
-            className="button button-round button--secondary"
-            href={'/login'}
-            target="_self"
-            rel="noopener noreferrer"
-          >
-            Log in
-          </Link>
-          <Link
-            className="button button-round button--highlight"
-            href={'/signin'}
-            target="_self"
-            rel="noopener noreferrer"
-          >
-            Sign in
-          </Link>
-        </div>
-        </div>
-      </section>
-    </main>
+      <PatternControl paused={paused} onToggle={() => setPaused(!paused)} />
+      <main className="container container-two">
+        <section className="column">
+          <DiceScene />
+        </section>
+        <section className="column gap-8">
+          <div className="row">
+            <h1>This is Dice</h1>
+            <p>Welcome</p>
+            <div className="flex flex-row gap-4 text-base font-medium">
+              <Link
+                className="button button-round button--secondary"
+                href="/login"
+                target="_self"
+                rel="noopener noreferrer"
+              >
+                Log in
+              </Link>
+              <Link
+                className="button button-round button--highlight"
+                href="/signin"
+                target="_self"
+                rel="noopener noreferrer"
+              >
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
