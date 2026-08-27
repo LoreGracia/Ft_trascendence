0f9b7796b9cabfb2863e6ca595e9f808a1433c1c
amarinite <63521854+amarinite@users.noreply.github.com>
2026-08-27 13:07:36 +0200

    2. base de datos (#114)
    * feat: postgresql database
    
    * fix: one env file for all vars and make down now doesn't remove images for quick re-deploys
    
    * examples for .env.local config vars
    
    * prisma schema and integration added as an external volume
    
    ---------
    
    Co-authored-by: Alba Marquez <amarquez@cbr4s5.42barcelona.com>
    Co-authored-by: Alba Marquez <amarquez@car9s4.42barcelona.com>
    Co-authored-by: «amarinite» <«albamrquz3@gmail.com»>

---
+++ srcs/app/api/test-db/route.ts
@@ -0,0 +1,12 @@
+// TEST - TO BE DELETED
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../../generated/prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return Response.json({ ok: true, users });
+}
