05b7c0934ee7f37c728a014bd6ffc4465e6e6687
Alba Marquez <amarquez@car10s6.42barcelona.com>
2026-09-01 12:10:16 +0200

    better auth sketch

--- srcs/app/api/auth/[...nextauth]/route.ts
+++ srcs/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,8 @@
-import { handlers } from "@/app/auth";
+// import { handlers } from "@/app/auth";
 
-export const { GET, POST } = handlers;
+// export const { GET, POST } = handlers;
+
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
+
+export const { GET, POST } = toNextJsHandler(auth);
