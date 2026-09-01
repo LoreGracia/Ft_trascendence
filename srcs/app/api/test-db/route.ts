1a0dca29ec58ea9a8548a047a5a97cb17708202a
amarinite <63521854+amarinite@users.noreply.github.com>
2026-09-01 15:35:18 +0200

    feat: rollback a versió estable
    * feat: postgresql database
    
    * fix: one env file for all vars and make down now doesn't remove images for quick re-deploys
    
    * examples for .env.local config vars
    
    * prisma schema and integration added as an external volume
    
    * cleaning prisma setup
    
    * export prisma client for next && skeleton better-auth instance
    
    * better auth dependencies installed
    
    * better auth sketch
    
    * better auth sketch
    
    * stick to the status quo
    
    * stick to the status quo
    
    * stick to the status quo
    
    ---------
    
    Co-authored-by: Alba Marquez <amarquez@cbr4s5.42barcelona.com>
    Co-authored-by: Alba Marquez <amarquez@car9s4.42barcelona.com>
    Co-authored-by: «amarinite» <«albamrquz3@gmail.com»>
    Co-authored-by: Alba Marquez <amarquez@car10s4.42barcelona.com>
    Co-authored-by: Alba Marquez <amarquez@car10s6.42barcelona.com>

--- srcs/app/api/test-db/route.ts
+++ srcs/app/api/test-db/route.ts
@@ -1,12 +1,12 @@
 // TEST - TO BE DELETED
 
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "../../../generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// const prisma = new PrismaClient({ adapter });
 
-export async function GET() {
-  const users = await prisma.user.findMany();
-  return Response.json({ ok: true, users });
-}
+// export async function GET() {
+//   const users = await prisma.user.findMany();
+//   return Response.json({ ok: true, users });
+// }
