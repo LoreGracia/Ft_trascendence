f8aa41ace3e51be5f9a5861da227a7104fec2daa
Alba Marquez <amarquez@car10s6.42barcelona.com>
2026-09-01 13:56:44 +0200

    stick to the status quo

--- srcs/app/api/auth/[...nextauth]/route.ts
+++ srcs/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
-// import { handlers } from "@/app/auth";
+import { handlers } from "@/app/auth";
 
-// export const { GET, POST } = handlers;
+export const { GET, POST } = handlers;
 
-import { auth } from "@/lib/auth";
-import { toNextJsHandler } from "better-auth/next-js";
+// import { auth } from "@/lib/auth";
+// import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth);
+// export const { GET, POST } = toNextJsHandler(auth);
