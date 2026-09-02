06bebab5cd8ad0d8977dd0b2e02c0310cad4fdf9
eina <eina@eina.cc>
2026-09-02 17:42:23 +0200

    fix: definir la política de CORS amb l'adreça al .env

--- server/sockets/index.ts
+++ server/sockets/index.ts
@@ -36,7 +36,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000",
 		methods: ["GET", "POST"],
 	},
 });
