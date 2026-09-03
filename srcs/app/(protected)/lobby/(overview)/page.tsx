e58ac20c8456bb69a2197fc965a0c04569ee450b
lgracia- <lgracia-@car10s2.42barcelona.com>
2026-09-03 16:15:58 +0200

    Version estabilizada con main

--- srcs/app/(protected)/lobby/(overview)/page.tsx
+++ srcs/app/(protected)/lobby/(overview)/page.tsx
@@ -1,19 +1,6 @@
-import { RoomCode, ExitButton } from "@/components/SocketComponent";
-import { redirect } from "next/navigation";
-import GameClient from "@/components/GameComponent";
 import GameRoom from "@/components/GameRoom";
-import type { GameType } from '@/types/game';
 
 export default function Lobby() {
-//   searchParams,
-// }: {
-//   searchParams?: Promise<{ roomCode?: string }>;
-// }) {
-//   const params = await searchParams;
-//   const roomCode = params?.roomCode ?? "";
-  // if (!roomCode) {
-  //   redirect("/login");
-  // }
   return (
 	<>
     {/* <GameClient/> */}
