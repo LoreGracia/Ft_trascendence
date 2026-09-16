e98454d1ee437deab5a2078d133b9d7c57752ec9
eina <git@eina.cc>
2026-09-16 14:59:03 +0200

    feat: seny i ordre, https i tota la pesca
    he netejat una mica fitxers, he unificat gitignores, ordenat l'env i creat la norma per generar certificats https així com el contenidor de traefik que ens fa de proxy per no haver de tocar el que ja està fet.
    ha estat un plaer (ha estat horrible).

--- srcs/lib/auth.ts
+++ srcs/lib/auth.ts
@@ -15,7 +15,7 @@ export const auth = betterAuth({
     enabled: true, 
   },
 
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_URL,
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
