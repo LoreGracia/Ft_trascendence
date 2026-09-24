5016fd00f2e6c1864195c3ed9e61576b4a50cd09
eina <git@eina.cc>
2026-09-24 17:59:51 +0200

    feat: prod build i domini

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  output: "standalone",
+  typescript: {ignoreBuildErrors: true},
+  allowedDevOrigins: ['dice.eina.cc'],
 };
 
 export default nextConfig;
