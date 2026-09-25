2e4015066101109fa3dd0bce92fc520dda45a3d0
eina <git@eina.cc>
2026-09-25 11:38:37 +0200

    multidomain

--- next/next.config.ts
+++ next/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   typescript: {ignoreBuildErrors: true},
-  allowedDevOrigins: ['dice.eina.cc'],
+  allowedDevOrigins: ['dice.eina.cc', '*.dice.eina.cc'],
 };
 
 export default nextConfig;
